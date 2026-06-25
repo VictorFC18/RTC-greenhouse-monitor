0675bb4fd16ec997b1ab8374fd20b47eae59857e
VictorFC18 <v.castro18.mc@gmail.com>
2026-06-25 16:09:28 -0500

    Inicializar proyecto y backend MQTT

---
+++ backend/mqtt_subscriber.py
@@ -0,0 +1,69 @@
+import paho.mqtt.client as mqtt
+import json
+# ==============================
+# CONFIGURACIÓN MQTT
+# ==============================
+
+BROKER = "192.168.100.95"
+PORT = 1883
+TOPIC = "invernadero/sensores"
+
+# ==============================
+# CALLBACK DE CONEXIÓN
+# ==============================
+
+def on_connect(client, userdata, flags, reason_code, properties=None):
+
+    if reason_code == 0:
+        print("Conectado al broker MQTT")
+
+        client.subscribe(TOPIC)
+
+        print(f"Suscrito a: {TOPIC}")
+
+    else:
+        print(f"Error de conexión: {reason_code}")
+
+# ==============================
+# CALLBACK DE MENSAJES
+# ==============================
+
+def on_message(client, userdata, msg):
+
+    try:
+
+        mensaje = msg.payload.decode()
+        datos = json.loads(mensaje)
+
+        print("\n===== NUEVA LECTURA =====")
+
+        print(f"Temperatura     : {datos['temperatura']} °C")
+        print(f"Humedad aire    : {datos['humedad']} %")
+        print(f"Humedad suelo   : {datos['suelo']} %")
+        print(f"pH              : {datos['ph']}")
+        print(f"Timestamp ESP32 : {datos['timestamp']}")
+
+        print()
+
+    except Exception as e:
+
+        print(f"Error al procesar el mensaje: {e}")
+
+# ==============================
+# CLIENTE MQTT
+# ==============================
+
+client = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2)
+
+client.on_connect = on_connect
+client.on_message = on_message
+
+# ==============================
+# CONEXIÓN
+# ==============================
+
+print("Conectando al broker...")
+
+client.connect(BROKER, PORT)
+
+client.loop_forever()
